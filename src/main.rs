e65b75f95f57b2f2681e01f155159b6673acca70
senmeannrealwat <meann.sen@realwat.net>
2024-10-03 12:30:19 +0700

    update

--- src/main.rs
+++ src/main.rs
@@ -21,12 +21,12 @@ fn main() {
 
     if command == "decode" {
         // You can use print statements as follows for debugging, they'll be visible when running tests.
-        println!("Logs from your program will appear here!");
+        eprintln!("Logs from your program will appear here!");
 
         let encoded_value = &args[2];
         let decoded_value = decode_bencoded_value(encoded_value);
-        println!("{}", decoded_value.to_string());
+        eprintln!("{}", decoded_value.to_string());
     } else {
-        println!("unknown command: {}", args[1])
+        eprintln!("unknown command: {}", args[1])
     }
 }
