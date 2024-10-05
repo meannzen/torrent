8f004703816a8d0bba2a9c7f8b3689fe8d070ee0
senmeannrealwat <meann.sen@realwat.net>
2024-10-05 11:35:51 +0700

    Calculate info hash

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use anyhow::Context;
 use clap::{Parser, Subcommand};
 use core::panic;
-use serde::Deserialize;
+use serde::{de::Visitor, Deserialize, Serialize};
+use sha1::{Digest, Sha1};
 use std::path::PathBuf;
 
 #[derive(Debug, Clone, Deserialize)]
@@ -11,13 +12,58 @@ struct Torrent {
     info: Info,
 }
 
-#[derive(Debug, Clone, Deserialize)]
+#[derive(Debug, Clone, Deserialize, Serialize)]
 #[allow(dead_code)]
 struct Info {
     name: String,
     #[serde(rename = "piece length")]
     plength: usize,
+    pieces: Hashes,
     length: usize,
+}
+
+#[derive(Debug, Clone)]
+struct Hashes(Vec<[u8; 20]>);
+struct HashesVisitor;
+
+impl<'d> Visitor<'d> for HashesVisitor {
+    type Value = Hashes;
+    fn visit_bytes<E>(self, v: &[u8]) -> Result<Self::Value, E>
+    where
+        E: serde::de::Error,
+    {
+        if v.len() % 20 != 0 {
+            return Err(E::custom(format!("length is {}", v.len())));
+        }
+        Ok(Hashes(
+            v.chunks_exact(20)
+                .map(|s| s.try_into().expect("to be length 2o"))
+                .collect(),
+        ))
+    }
+
+    fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+        formatter.write_str("a byte string length is a mutiple of 20")
+    }
+}
+
+impl<'de> Deserialize<'de> for Hashes {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        deserializer.deserialize_bytes(HashesVisitor)
+    }
+}
+
+impl Serialize for Hashes {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        let signle_slice = self.0.concat();
+        serializer.serialize_bytes(&signle_slice)
+    }
 }
 
 fn decode_bencoded_value(encoded_value: &str) -> (serde_json::Value, &str) {
@@ -112,6 +158,12 @@ fn main() -> anyhow::Result<()> {
 
             println!("Tracker URL: {}", t.announce);
             println!("Length: {}", t.info.length);
+
+            let info_encode = serde_bencode::to_bytes(&t.info).context("re-encode info section")?;
+            let mut hasher = Sha1::new();
+            hasher.update(&info_encode);
+            let info_hash = hasher.finalize();
+            println!("Info Hash: {}", hex::encode(&info_hash));
         }
     }
 
